ae99df5b69a46aa814b02ce41c1b4b6f9b7ff6fb
Marcel <numax.cz@protonmail.ch>
2023-05-28 17:51:01 +0200

    Update

--- NapicuFridge/src/app/home/home.page.ts
+++ NapicuFridge/src/app/home/home.page.ts
@@ -1,13 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {BluetoothSerial} from "@ionic-native/bluetooth-serial";
-import {AndroidPermissions} from "@awesome-cordova-plugins/android-permissions";
-import {AlertController, NavController, ToastController} from "@ionic/angular";
+import {Component} from '@angular/core';
 
-interface pairedlist {
-  "class": number,
-  "id": string,
-  "address": string,
-  "name": string
+import {BLE} from "@awesome-cordova-plugins/ble";
+
+interface data {
+  name: string,
+  id: string,
+  advertising: ArrayBuffer,
+  rssi: number
 }
 
 
@@ -18,124 +17,25 @@ interface pairedlist {
 })
 export class HomePage {
 
-  public unpairedDevices: any;
-  public pairedDevices: any;
-  public declare gettingDevices: Boolean;
+  public devicess: data[] = [];
 
-  constructor(
-    public alertCtrl: AlertController
-  ) {
-    let obj = this;
+  public error: any = "";
 
-    // Check, if Bluetooth is enabled:
-    BluetoothSerial.isEnabled().then(
-      (result: any) => {
-        console.log(result);
-
-      },
-      (error: any) => {
-        console.log(error);
-
-        obj._enableBluetooth();
-      }
-    );
+  constructor() {
   }
 
-  private _enableBluetooth(): void {
-    BluetoothSerial.enable().then(
-      (success: any) => {
-        console.log(success);
+  public scan(): void {
+    BLE.startScan([]).subscribe(
+      {
+        next: (data: data) => {
+          this.devicess.push(data);
+        },
 
-      },
-      (error: any) => {
-        console.log(error);
       }
-    );
-  }
+    )
 
-  public startScanning(): void {
-    this.pairedDevices = null;
-    this.unpairedDevices = null;
-    this.gettingDevices = true;
+    //TODO END scan + services
 
-    BluetoothSerial.discoverUnpaired().then(
-      (result: any) => {
-        console.log(result);
-
-        this.unpairedDevices = result;
-        this.gettingDevices = false;
-
-        result.forEach((element: any) => {
-          console.log(element);
-        });
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
-
-    BluetoothSerial.list().then(
-      (result: any) => {
-        console.log(result);
-
-        this.pairedDevices = result;
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
-  }
-
-  public selectDevice(address: any): any {
-    // console.log(address);
-    //
-    // let alert = this.alertCtrl.create({
-    //   title:    'Connect?',
-    //   message:  'Do you want to connect to the device?',
-    //   buttons:  [
-    //     {
-    //       text: 'Cancel',
-    //       role: 'cancel',
-    //       handler: () => {
-    //         console.log('Cancel clicked.');
-    //       }
-    //     },
-    //     {
-    //       text: 'Connect',
-    //       handler: () => {
-    //         BluetoothSerial.connectInsecure(address).subscribe(this._success, this._fail);
-    //       }
-    //     }
-    //   ]
-    // });
-    // alert.present();
-  }
-
-  private _success = (data: any) => console.log(data);
-
-  private _fail = (error: any) => console.log(error);
-
-  public async disconnect() {
-    // let alert = this.alertCtrl.create({
-    //   title:    'Disconnect?',
-    //   message:  'Do you want to disconnect from the device?',
-    //   buttons:  [
-    //     {
-    //       text: 'Cancel',
-    //       role: 'cancel',
-    //       handler: () => {
-    //         console.log('Cancel clicked.');
-    //       }
-    //     },
-    //     {
-    //       text: 'Disconnect',
-    //       handler: () => {
-    //         BluetoothSerial.disconnect();
-    //       }
-    //     }
-    //   ]
-    // });
-    // alert.present();
   }
 
 }
