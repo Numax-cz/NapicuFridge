8f484fdd308b74043765cda301a71b75e8c7a4ea
Marcel <numax.cz@protonmail.ch>
2023-08-24 10:32:22 +0200

    Importována charakteristika

--- NapicuFridge/src/app/config/configuration.ts
+++ NapicuFridge/src/app/config/configuration.ts
@@ -4,4 +4,5 @@ export namespace Configuration {
   export const SERVICE_UUID: string = "5ff8dace-0914-11ee-be56-0242ac120002";
   export const CHARACTERISTIC_DISPLAY_ENABLE_UUID: string = "5ff8dda8-0914-11ee-be56-0242ac120002";
   export const CHARACTERISTIC_DHT_INSIDE_UUID: string = "5ff8e046-0914-11ee-be56-0242ac120002";
+  export const CHARACTERISTIC_DHT_OUTSIDE_UUID: string =  "be3cd53c-4257-11ee-be56-0242ac120002"
 }
